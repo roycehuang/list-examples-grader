c89041ec34fd014d0e0ef015d16bcfded3c079df
roycehuang <130111798+roycehuang@users.noreply.github.com>
2023-05-25 16:24:38 -0700

    new

--- grade.sh
+++ grade.sh
@@ -1,4 +1,5 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+#set -e
+CPATH = '.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 rm -rf grading-area
@@ -8,7 +9,27 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
-if [[-e ListExamples.java]]
+if [ -e ./student-submission/ListExamples.java ]
+then 
+    echo "Correct file!"
+    cp -r ./student-submission/ListExamples.java ./grading-area/
+else
+    echo "Wrong file!"
+    exit 1
+fi
+
+if ! javac -cp $CPATH -d ./student-submission/TestListExamples.java
+then
+    echo 'Compilation of tests failed!'
+    exit 1
+fi
+
+if ! javac -cp $CPATH -d ./grading-area/ListExamples.java
+then
+    echo "Compilation of student's code failed!"
+    exit 1
+fi
+
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
